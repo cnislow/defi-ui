18c739a2b3ede44c167b6019222aa2033d4491b6
Chad Nislow <cjnislow@gmail.com>
2024-03-15 13:58:33 -0400

    uses bootstrap to setup UI to change greeting value and deposit eth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const[greet, setGreet] = useState('');
+  const [depositValue, setDepositValue] = useState('')
+  const [greetingValue, setGreetingValue] = useState('')
+
+  const handleDepositChange = (e) => {
+    setDepositValue(e.target.value);
+  }
+
+  const handleGreetingChange = (e) => {
+    setGreetingValue(e.target.value);
+  }
+
+  const handleDepositSubmit = (e) => {
+    e.preventDefault();
+    console.log(depositValue);
+  }
+
+  const handleGreetingSubmit = (e) => {
+    e.preventDefault();
+    console.log(greetingValue);
+  }
+
   return (
-    <h1>Hello</h1>
+    <div className="container">
+      <div className="container"> 
+        <div className="row mt-5">
+          <div className="col">
+            <h3>Greeting</h3>
+            <p>Contract balance: 0</p>
+          </div>
+          <div className="col">
+          <form onSubmit={handleDepositSubmit} className="mt-5">
+            <div className="mb-3">
+              <input type="number" className="form-control" placeholder="0" onChange={handleDepositChange} value={depositValue} />
+            </div>
+            <button type="submit" className="btn btn-success">Deposit</button>
+          </form>
+          <form onSubmit={handleGreetingSubmit} className="mt-5">
+            <div className="mb-3">
+              <input type="text" className="form-control" onChange={handleGreetingChange} value={greetingValue} />
+            </div>
+            <button type="submit" className="btn btn-dark">Change</button>
+          </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
